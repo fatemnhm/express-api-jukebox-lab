87ee8daf312e07744af70ed74de4ef6c11295914
fatemnhm <fatemanhm@gmail.com>
2024-11-23 20:47:56 +0300

    done

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -22,6 +22,43 @@ router.get('/', async (req, res) => {
     }
 });
 
+// READ ONE TRACK - GET - /tracks/:trackId
+router.get('/:trackId', async (req, res) => {
+    try {
+        const foundTrack = await Track.findById(req.params.trackId);
+        if (!foundTrack) {
+            res.status(404);
+            throw new Error('Track not found.');
+        }
+        res.status(200).json(foundTrack);
+    } catch (error) {
+        if (res.statusCode === 404) {
+            res.json({ error: error.message });
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+    }
+});
+// UPDATE - PUT - /tracks/:trackId
+
+router.put('/:trackId', async (req, res) => {
+    try {
+      const updatedTrack = await Track.findByIdAndUpdate(req.params.trackId, req.body, {
+          new: true
+      })
+      if (!updatedTrack) {
+          res.status(404)
+          throw new Error('Track not found')
+      }
+      res.status(200).json(updatedTrack)
+    } catch (error) {
+      if (res.statusCode === 404) {
+          res.json({ error: error.message })
+      }
+      res.status(500).json({ error: error.message })
+    }
+  })
+  
 // DELETE - DELETE - /tracks/:trackId
 router.delete('/:trackId', async (req, res) => {
     try {
